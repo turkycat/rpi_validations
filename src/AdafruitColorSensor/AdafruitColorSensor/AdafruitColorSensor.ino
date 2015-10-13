4ac0421e6bf5bb3f9785c8042f95d6025894d383
Jesse Frush <jesse.frush@gmail.com>
2015-10-13 15:13:46 -0700

    Adding Adafruit ADXL345 color sensor solution

---
+++ src/AdafruitColorSensor/AdafruitColorSensor/AdafruitColorSensor.ino
@@ -0,0 +1,89 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+// Pick analog outputs, for the UNO these three work well
+// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
+#define redpin 3
+#define greenpin 5
+#define bluepin 6
+// for a common anode LED, connect the common pin to +5V
+// for common cathode, connect the common to ground
+
+// set to false if using a common cathode LED
+#define commonAnode true
+
+// our RGB -> eye-recognized gamma color
+byte gammatable[256];
+
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725( TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X );
+
+void setup() {
+    Log(  "Color View Test!"  ); Log( "\n" );
+
+    if( tcs.begin() ) {
+        Log(  "Found sensor"  ); Log( "\n" );
+    }
+    else {
+        Log(  "No TCS34725 found ... check your connections"  ); Log( "\n" );
+        while( 1 ); // halt!
+    }
+
+    // use these three pins to drive an LED
+    pinMode( redpin, OUTPUT );
+    pinMode( greenpin, OUTPUT );
+    pinMode( bluepin, OUTPUT );
+
+    // thanks PhilB for this gamma table!
+    // it helps convert RGB colors to what humans see
+    for( int i = 0; i<256; i++ ) {
+        float x = i;
+        x /= 255;
+        x = pow( x, 2.5 );
+        x *= 255;
+
+        if( commonAnode ) {
+            gammatable[i] = 255 - x;
+        }
+        else {
+            gammatable[i] = x;
+        }
+        //Log( gammatable[i] ); Log( "\\n" );
+    }
+}
+
+
+void loop() {
+    uint16_t clear, red, green, blue;
+
+    tcs.setInterrupt( false );      // turn on LED
+
+    delay( 60 );  // takes 50ms to read 
+
+    tcs.getRawData( &red, &green, &blue, &clear );
+
+    tcs.setInterrupt( true );  // turn off LED
+
+    Log( "C:\t" ); Log( clear.ToString()->Begin() );
+    Log( "\tR:\t" ); Log( red.ToString()->Begin() );
+    Log( "\tG:\t" ); Log( green.ToString()->Begin() );
+    Log( "\tB:\t" ); Log( blue.ToString()->Begin() );
+
+    // Figure out some basic hex code for visualization
+    uint32_t sum = clear;
+    float r, g, b;
+    r = red; r /= sum;
+    g = green; g /= sum;
+    b = blue; b /= sum;
+    r *= 256; g *= 256; b *= 256;
+    Log( "\t" );
+    Log( r.ToString()->Begin() ); Log( g.ToString()->Begin() ); Log( b.ToString()->Begin() );
+    Log( "\n" );
+
+    //Log((int)r ); Log(" "); Log((int)g);Log(" ");  Log( (int)b  ); Log( "\\n" );
+
+    analogWrite( redpin, gammatable[(int)r] );
+    analogWrite( greenpin, gammatable[(int)g] );
+    analogWrite( bluepin, gammatable[(int)b] );
+}
+
