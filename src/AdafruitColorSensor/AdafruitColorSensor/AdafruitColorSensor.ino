7d41cdf9dca624822f95b518134abddcbf9e73b2
Jesse Frush <jesse.frush@gmail.com>
2015-10-14 15:22:14 -0700

    Updated AdafruitColorSensor solution with latest nuget, sketch changes

--- src/AdafruitColorSensor/AdafruitColorSensor/AdafruitColorSensor.ino
+++ src/AdafruitColorSensor/AdafruitColorSensor/AdafruitColorSensor.ino
@@ -1,20 +1,11 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
-// Pick analog outputs, for the UNO these three work well
-// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
-//#define redpin 3
-//#define greenpin 5
-//#define bluepin 6
-// for a common anode LED, connect the common pin to +5V
-// for common cathode, connect the common to ground
-
 // set to false if using a common cathode LED
 #define commonAnode true
 
 // our RGB -> eye-recognized gamma color
 byte gammatable[256];
-
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725( TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X );
 
@@ -28,11 +19,6 @@ void setup() {
         Log( "No TCS34725 found ... check your connections" ); Log( "\n" );
         while( 1 ); // halt!
     }
-
-    // use these three pins to drive an LED
-    //pinMode( redpin, OUTPUT );
-    //pinMode( greenpin, OUTPUT );
-    //pinMode( bluepin, OUTPUT );
 
     // thanks PhilB for this gamma table!
     // it helps convert RGB colors to what humans see
--- src/LiquidCrystalDisplay/LiquidCrystalDisplay/LiquidCrystalDisplay.ino
+++ src/LiquidCrystalDisplay/LiquidCrystalDisplay/LiquidCrystalDisplay.ino
@@ -1,60 +1,22 @@
-/*
-LiquidCrystal Library - Hello World
-
-Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
-library works with all LCD displays that are compatible with the
-Hitachi HD44780 driver. There are many of them out there, and you
-can usually tell them by the 16-pin interface.
-
-This sketch prints "Hello World!" to the LCD
-and shows the time.
-
-The circuit:
-* LCD RS pin to digital pin 12
-* LCD Enable pin to digital pin 11
-* LCD D4 pin to digital pin 5
-* LCD D5 pin to digital pin 4
-* LCD D6 pin to digital pin 3
-* LCD D7 pin to digital pin 2
-* LCD R/W pin to ground
-* LCD VSS pin to ground
-* LCD VCC pin to 5V
-* 10K resistor:
-* ends to +5V and ground
-* wiper to LCD VO pin (pin 3)
-
-Library originally added 18 Apr 2008
-by David A. Mellis
-library modified 5 Jul 2009
-by Limor Fried (http://www.ladyada.net)
-example added 9 Jul 2009
-by Tom Igoe
-modified 22 Nov 2010
-by Tom Igoe
-
-This example code is in the public domain.
-
-http://www.arduino.cc/en/Tutorial/LiquidCrystal
-*/
-
-// include the library code:
 #include <LiquidCrystal.h>
 
 int enablePin = GPIO_26;
-int registerSelectPin = GPIO_6;
+int registerSelectPin = GPIO_20;
 int dataPin11 = GPIO_2;
 int dataPin12 = GPIO_3;
 int dataPin13 = GPIO_4;
 int dataPin14 = GPIO_5;
 
-// initialize the library with the numbers of the interface pins
-//LiquidCrystal lcd( registerSelectPin, enablePin, dataPin11, dataPin12, dataPin13, dataPin14 );
-LiquidCrystal *lcd;// ( registerSelectPin, enablePin, dataPin11, dataPin12, dataPin13, dataPin14 );
+//create a pointer to an instance of LiquidCrystal, yet to be created
+LiquidCrystal *lcd;
 
 void setup() {
+    //create the LiquidCrystal instance with the pins as set
     lcd = new LiquidCrystal( registerSelectPin, enablePin, dataPin11, dataPin12, dataPin13, dataPin14 );
+
     // set up the LCD's number of columns and rows:
     lcd->begin( 16, 2 );
+
     // Print a message to the LCD.
     lcd->print( "hello, world!" );
 }
@@ -63,6 +25,7 @@ void loop() {
     // set the cursor to column 0, line 1
     // (note: line 1 is the second row, since counting begins with 0):
     lcd->setCursor( 0, 1 );
+
     // print the number of seconds since reset:
     lcd->print( millis() / 1000 );
 }
